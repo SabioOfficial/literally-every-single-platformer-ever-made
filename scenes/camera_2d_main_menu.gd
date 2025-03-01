5c2c5d7d6bbeb70d6ced41714ba468c8024d23f4
Sabio <gamester.sad@gmail.com>
2025-03-01 18:52:23 +0000

    disabled level 3 dialogue and added cool camera in main menu

---
+++ scenes/camera_2d_main_menu.gd
@@ -0,0 +1,14 @@
+extends Camera2D
+
+func cameraUpdate():
+	var pos = get_local_mouse_position()
+	if (pos.x >= -.1 || pos.x <= .1):
+		set_position(pos)
+		
+	if (pos.x < 286):
+		$"../CharacterBody2D/Sprite2D".flip_h = true
+	else:
+		$"../CharacterBody2D/Sprite2D".flip_h = false
+
+func _process(delta: float) -> void:
+	cameraUpdate()
