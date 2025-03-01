c97d46dbe724be2bdfbdbf5c144a66f9e1fca95c
Sabio <gamester.sad@gmail.com>
2025-03-01 12:44:53 +0000

    Main menu music + animations + put levels into its own folder, added plugin "Dialogue Manager"

--- scenes/banana-level-2.gd
+++ scenes/banana-level-2.gd
@@ -3,4 +3,4 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
 		queue_free()
-		get_tree().change_scene_to_file("res://scenes/i-love-difficulty-spikes.tscn")
+		get_tree().change_scene_to_file("res://scenes/levels/level-2.tscn")
