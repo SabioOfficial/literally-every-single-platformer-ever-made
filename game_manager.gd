1fbd0dc29168c0f1881a6acc9034cf93d6ce19b3
Sabio <gamester.sad@gmail.com>
2025-03-01 13:22:33 +0000

    dialogue for level 3
    level 3 :O

--- game_manager.gd
+++ game_manager.gd
@@ -6,6 +6,10 @@ var potassiums = 0
 
 var additionalspeed = 10
 
+func _ready() -> void:
+	DialogueManager.show_example_dialogue_balloon(load("res://dialogue/level-3-start.dialogue"), "start")
+	return
+
 func add_potassium():
 	potassiums += 1
 	print(potassiums)
