d1a24b9f6766bda0fe39cbffea68f8f296f8b5c0
Sabio <gamester.sad@gmail.com>
2025-03-02 18:53:28 +0000

    enemy movement!

--- assets/scenes/subscenes/ghost.gd
+++ assets/scenes/subscenes/ghost.gd
@@ -1,6 +1,9 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @onready var game_manager: Node = %GameManager
+
+# func _process(delta: float) -> void:
+	#
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
@@ -15,3 +18,24 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 				body.knockback(500)
 			else:
 				body.knockback(-500)
+
+var is_moving_left = true
+
+var gravity =  10 # check https://www.youtube.com/watch?v=jQKxOEbbirA for more detail
+
+var speed = 32 # pixels per second
+
+func _process(_delta):
+	move_character()
+	detect_turn_around()
+		
+func move_character():
+	velocity.x = -speed if is_moving_left else speed
+	velocity.y += gravity
+	
+	move_and_slide()
+
+func detect_turn_around():
+	if not $RayCast2D.is_colliding() and is_on_floor():
+		is_moving_left = !is_moving_left
+		scale.x = -scale.x
