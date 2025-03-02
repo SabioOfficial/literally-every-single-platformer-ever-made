ebd1d9b28d0ba590e1b19b5f45ce59583434dc12
Sabio <gamester.sad@gmail.com>
2025-03-02 07:57:21 +0000

    updated camera flip h controls

--- scenes/banana-level-2.gd
+++ scenes/banana-level-2.gd
@@ -2,5 +2,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "CharacterBody2D"):
-		queue_free()
 		get_tree().change_scene_to_file("res://scenes/levels/level-2.tscn")
--- scenes/camera_2d_main_menu.gd
+++ scenes/camera_2d_main_menu.gd
@@ -1,14 +1,24 @@
 extends Camera2D
 
+@onready var sprite_2d: AnimatedSprite2D = $"../CharacterBody2D/Sprite2D"
+
+func _ready() -> void:
+	print("Start")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	# await $"../../UI/Main/AnimationPlayer".animation_finished
+	# print("Anim finished")
+	await get_tree().create_timer(2.5).timeout
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
 func cameraUpdate():
-	var pos = get_local_mouse_position()
-	if (pos.x >= -.1 || pos.x <= .1):
-		set_position(pos)
-		
-	if (pos.x < 286):
-		$"../CharacterBody2D/Sprite2D".flip_h = true
+	var mousepos = get_local_mouse_position()
+	if (mousepos.x >= -.1 || mousepos.x <= .1):
+		set_position(mousepos)
+	
+	if (mousepos.x + 286 < $"../CharacterBody2D".position.x): # 286 is the center of the screen.
+		sprite_2d.flip_h = true
 	else:
-		$"../CharacterBody2D/Sprite2D".flip_h = false
+		sprite_2d.flip_h = false
 
 func _process(_delta: float) -> void:
 	cameraUpdate()
